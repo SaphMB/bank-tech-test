66aff496814630288c0116ba483865a4f2ebcbab
Sapphire Mason-Brown <saphmb@gmail.com>
2017-09-04 16:45:22 +0100

    Account: deposit method updates balance and tranasctions array

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,29 @@
 class Account
   DEFAULT_BALANCE = 0
 
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize
     @transactions = []
     @balance = DEFAULT_BALANCE
   end
+
+  def deposit(deposit)
+    increase_balance(deposit)
+    add_transaction(deposit)
+  end
+
+  private
+
+  attr_writer :balance, :transactions
+
+  def increase_balance(deposit)
+    @balance += deposit.amount
+  end
+
+  def add_transaction(transaction)
+    @transactions << transaction
+  end
+
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,10 +2,27 @@ require 'account'
 
 describe Account do
   subject(:account) { described_class.new }
+  let(:deposit) { double 'deposit' }
 
   describe 'upon initialization' do
     it 'should have a balance of 0' do
       expect(account.balance).to eq 0
+    end
+  end
+
+  describe '#deposit' do
+
+    before do
+      allow(deposit).to receive(:amount) { 50 }
+    end
+
+    it 'updates the balance by the deposit amount' do
+      expect { account.deposit(deposit) } .to change { account.balance } .by(50)
+    end
+
+    it 'adds the deposit to the transactions' do
+      account.deposit(deposit)
+      expect(account.transactions).to include deposit
     end
   end
 end
